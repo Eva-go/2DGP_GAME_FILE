08c0011305c2e70854cc30455b0309fa6cce5e69
JYH <whdudghks22@gmail.com>
2019-10-18 15:38:57 +0900

    커밋2회
    커밋2회

--- 2DGP_Game.py
+++ 2DGP_Game.py
@@ -1,15 +1,43 @@
 from pico2d import *
 
+
+# Game object class here
+class Player:
+    def __init__(self):
+        self.x, self.y = 1000, 1000
+        self.image = load_image('Ironclad.png')
+
+    def draw(self):
+        self.image.draw(200, 150)
+    def update(self):
+        pass
+
+
+
+class Monster:
+    pass
+
+
+def handle_events():
+    global Game_Strat
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            Game_Strat = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            Game_Strat = False
+
+
+# initialization code 초기화
 open_canvas()
-ironclad = load_image('ironclad.png')
-x = 0
-frame = 0
-while x < 800:
+player = Player()
+Game_Strat = True
+while Game_Strat:
+    handle_events()
+    player.update()
     clear_canvas()
-    ironclad.clip_draw(frame * 100, 0, 100, 100, x, 90)
+    player.draw()
+
+    delay(0.01)
     update_canvas()
-    frame = (frame + 1) % 15
-    x += 5
-    get_events()
-    delay(0.1)
 close_canvas()
